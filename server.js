f0f43fc467e017cc64759239410ed9767744ed6b
Fran Josipović <fran.josipovic@eviden.com>
2025-04-09 23:23:20 +0200

    add dynamic img loading

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+// app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+const fs = require("fs");
+const path = require("path");
+
+app.get("/slike", (req, res) => {
+  const dataPath = path.join(__dirname, "images.json");
+  const images = JSON.parse(fs.readFileSync(dataPath));
+  res.render("slike", { images });
+});
+
+app.listen(3000, () => {
+  console.log("Server pokrenut na http://localhost:3000");
+});
